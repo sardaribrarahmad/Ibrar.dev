86fece876e29ac4ce9f4fc9380e53e9c2fa78182
Sardar Ibrar Ahmad <sardaribrar7688@gmail.com>
2024-11-28 16:14:47 +0500

    5th changes

--- src/components/sections/about-me.tsx
+++ src/components/sections/about-me.tsx
@@ -29,36 +29,28 @@ const AboutMeSection = () => {
         </div>
 
         {/* Content */}
-        <div className="flex max-w-xl flex-col gap-6">
+        <div className="flex max-w-xl flex-col gap-6 text-justify">
           <Typography variant="h3">
             Curious about me? Here you have it:
           </Typography>
           <Typography>
-            I&apos;m a passionate, self-proclaimed designer who specializes in
-            full stack development (React.js & Node.js). I am enthusiastic about
+            I&apos;m a passionate, self-proclaimed developer who specializes in
+            frontend development (React.js & Next.js). I am enthusiastic about
             bringing the technical and visual aspects of digital products to
             life. User experience, pixel perfect design, and writing clear,
             readable, highly performant code matters to me.
           </Typography>
           <Typography>
-            I began my journey as a web developer in 2015, and since then,
+            I began my journey as a web developer in 2020, and since then,
             I&apos;ve continued to grow and evolve as a developer, taking on new
             challenges and learning the latest technologies along the way. Now,
-            in my early thirties, 7 years after starting my web development
+            in my early thirties, 3 years after starting my web development
             journey, I&apos;m building cutting-edge web applications using
-            modern technologies such as Next.js, TypeScript, Nestjs,
-            Tailwindcss, Supabase and much more.
+            modern technologies such as Next.js, TypeScript, Tailwindcss.
           </Typography>
           <Typography>
             I am very much a progressive thinker and enjoy working on products
             end to end, from ideation all the way to development.
-          </Typography>
-          <Typography>
-            When I&apos;m not in full-on developer mode, you can find me
-            hovering around on twitter or on indie hacker, witnessing the
-            journey of early startups or enjoying some free time. You can follow
-            me on where I share tech-related bites and build in public, or you
-            can follow me on{" "}
             <Link
               noCustomization
               externalLink
@@ -69,22 +61,19 @@ const AboutMeSection = () => {
             </Link>
             .
           </Typography>
+
           <Typography>Finally, some quick bits about me.</Typography>
           <div className="flex flex-col gap-2 md:flex-row md:gap-6">
             <ul className="flex list-inside list-disc flex-col gap-2">
-              <Typography component="li">
-                B.E. in Computer Engineering
-              </Typography>
-              <Typography component="li">Full time freelancer</Typography>
+              <Typography component="li">B.S in Computer Sciences</Typography>
             </ul>
             <ul className="flex list-inside list-disc flex-col gap-2">
               <Typography component="li">Avid learner</Typography>
-              <Typography component="li">Aspiring indie hacker</Typography>
             </ul>
           </div>
           <Typography>
             One last thing, I&apos;m available for freelance work, so feel free
-            to reach out and say hello! I promise I don&apos;t bite 😉
+            to reach out.
           </Typography>
         </div>
       </div>
