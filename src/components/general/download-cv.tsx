b4f15e5c69c810f82cb69c4e9966027f69f87e18
Sardar Ibrar Ahmad <sardaribrar7688@gmail.com>
2024-12-01 11:19:01 +0500

    12 changes

--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -4,9 +4,7 @@ import Button from '@/components/general/button';
 
 const DownloadCV = () => {
   return (
-    <Button
-      onClick={() => window?.open("/files/sardar_ibrar_resume.pdf", "_blank")}
-    >
+    <Button onClick={() => window?.open("/files/SardarIbrar.CV.pdf", "_blank")}>
       Download CV
     </Button>
   );
